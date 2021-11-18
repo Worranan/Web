2bda9c280305bebb7aab5c3653f6a0f4c64df188
Worranan <nun.45_w@hotmail.com>
2021-11-18 16:23:54 +0700

    again

--- flask_app.py
+++ flask_app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return 'Hello from Worra'
+        return 'Hello from Worra Jaaaa'
 @app.route("/aa")
 def hello_friend():
         return "Wow you are so great"
